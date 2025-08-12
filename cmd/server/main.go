0b9d4312613b9c4debb6ab6726c53d91c51f870b
Simon Cardona <simon.cdna@proton.me>
2025-08-12 23:50:17 +0200

    chore(backend): init go

---
+++ cmd/server/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Toolforge API is running",
+		})
+	})
+
+	err := router.Run()
+	if err != nil {
+		fmt.Println("router run error:", err)
+	}
+}
